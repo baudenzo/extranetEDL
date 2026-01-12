4545b7ee74e7c252c7ba470be45c6efec7f429fe
baudenzo <baudenzo21@gmail.com>
2026-01-12 09:30:03 +0100

    Remove sensitive files and update .gitignore

--- doc_perso.md
+++
@@ -1,9 +0,0 @@
-## Base de données
-- localhost / edl / root / (vide)
-
-## Utilisateurs
-- marie01 / Marie
-- pierre01 / Pierre
-- sophie01 / Sophie
-- lucas01 / Lucas
-
--- sql/script_insertion.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO utilisateurs (email, prenom, nom, numlogin, password, role, sexe, photo) VALUES
-('marie.martin@example.com', 'Marie', 'Martin', 'marie01', SHA2('Marie', 256), 'admin', 'feminin', 'pp/1.png'),
-('pierre.dubois@example.com', 'Pierre', 'Dubois', 'pierre01', SHA2('Pierre', 256), 'formateur', 'masculin', 'pp/2.jpg'),
-('sophie.garcia@example.com', 'Sophie', 'Garcia', 'sophie01', SHA2('Sophie', 256), 'stagiaire OP', 'feminin', 'pp/3.png'),
-('lucas.lopez@example.com', 'Lucas', 'Lopez', 'lucas01', SHA2('Lucas', 256), 'stagiaire FPC', 'masculin', 'pp/4.jpg');
-
